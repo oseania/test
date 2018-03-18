48e786759ded9b8740f6750cbb186dba15a3780b
oseania <tigermasksim@gmail.com>
2018-03-18 12:25:15 +0900

    현재시각출력

--- JavaTest/src/kr/co/test/HelloWorld.java
+++ JavaTest/src/kr/co/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!!");
-		System.out.println("hello java!!");
+		System.out.println("Time is 12:24!");
 	}
 
 }
