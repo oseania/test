b56743f180656167077e07cf6b4463a22ec507fb
oseania <tigermasksim@gmail.com>
2018-03-18 13:23:42 +0900

    test브랜치에 코드추가

--- JavaTest/src/kr/co/test/HelloWorld.java
+++ JavaTest/src/kr/co/test/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!!");
 		System.out.println("Time is 12:24!");
+		System.out.println("SO Hot");
 	}
 
 }
